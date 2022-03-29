eb295503553d8d6f532a6ce4ab8e799972c3d2a0
efildra <filip.drasic@ericsson.com>
2022-03-29 11:29:18 +0200

    Add elastic query web client microservice

---
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/exception/ElasticQueryWebClientException.java
@@ -0,0 +1,19 @@
+package com.microservices.demo.elastic.query.web.client.exception;
+
+public class ElasticQueryWebClientException extends RuntimeException
+{
+    public ElasticQueryWebClientException()
+    {
+        super();
+    }
+
+    public ElasticQueryWebClientException(String message)
+    {
+        super(message);
+    }
+
+    public ElasticQueryWebClientException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+}
---
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/model/ElasticQueryWebClientResponseModel.java
@@ -0,0 +1,21 @@
+package com.microservices.demo.elastic.query.web.client.model;
+
+import java.time.ZonedDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ElasticQueryWebClientResponseModel
+{
+    private String id;
+    private Long userId;
+    private String text;
+    private ZonedDateTime createdAt;
+}
+
