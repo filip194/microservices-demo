5e1c697ebd455be68494b81347007d634ac0f1d9
efildra <filip.drasic@ericsson.com>
2022-08-07 13:36:57 +0200

    Configure web client security with Spring boot security Oauth2, OIDC and JWT

--- elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/security/WebSecurityConfig.java
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/security/WebSecurityConfig.java
@@ -1,60 +1,131 @@
 package com.microservices.demo.elastic.query.web.client.security;
 
-import org.springframework.context.annotation.Bean;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 
-import com.microservices.demo.config.UserConfigData;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity // should be removed if using spring-boot-starter-oauth2-client
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
-    private final UserConfigData userConfigData;
+    private static final String GROUPS_CLAIM = "groups";
+    private static final String ROLE_PREFIX = "ROLE_";
 
-    public WebSecurityConfig(UserConfigData userConfigData)
+    //    private final UserConfigData userConfigData; // also removed when using spring-boot-starter-oauth2-client
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    @Value("${security.logout-success-url}")
+    private String logoutSuccessUrl;
+
+    public WebSecurityConfig(ClientRegistrationRepository clientRegistrationRepository)
     {
-        this.userConfigData = userConfigData;
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler()
+    {
+        final OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(
+                clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri(logoutSuccessUrl);
+        return successHandler;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
         http
-                .httpBasic()
-                .and()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/**").hasRole("USER")
                 .anyRequest()
                 .fullyAuthenticated()
                 .and()
                 .logout()
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID");
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                .and()
+                .oauth2Client()
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userAuthoritiesMapper(userAuthoritiesMapper());
+
+        // REMOVED AS WE ARE NOT USING IN-MEMORY AUTHENTICATION ANYMORE
+//                .httpBasic()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/")
+//                .permitAll()
+//                .antMatchers("/**")
+//                .hasRole("USER")
+//                .anyRequest()
+//                .fullyAuthenticated()
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/")
+//                .invalidateHttpSession(true)
+//                .deleteCookies("JSESSIONID");
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    // customize granted authorities with custom mapper
+    private GrantedAuthoritiesMapper userAuthoritiesMapper()
     {
-        auth
-                .inMemoryAuthentication()
-                .withUser(userConfigData.getUsername())
-                // .password(userConfigData.getPassword()) // using BCrypted and then encrypted password with JCE
-                .password(passwordEncoder().encode(userConfigData.getPassword())) // using encrypted password with JCE, preferred way as it is working in all cases, and the above on is not
-                .roles(userConfigData.getRoles());
+        // map our authorities to spring security authorities, so we can do authorization logic here in client also
+        // THIS WAY, AUTHENTICATED USER WILL HAVE ALL THE GROUPS FROM KEYCLOAK MAPPED TO ROLE_ AUTHORITIES IN SPRING CONTEXT
+        return (authorities -> {
+            final Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach(authority -> {
+                if (authority instanceof OidcUserAuthority)
+                {
+                    final OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
+                    final OidcIdToken oidcIdToken = oidcUserAuthority.getIdToken();
+                    log.info("Username from ID token: {}", oidcIdToken.getPreferredUsername());
+                    final OidcUserInfo oidcUserInfo = oidcUserAuthority.getUserInfo();
+
+                    final List<SimpleGrantedAuthority> groupAuthorities = oidcUserInfo
+                            .getClaimAsStringList(GROUPS_CLAIM).stream()
+                            .map(group -> new SimpleGrantedAuthority(ROLE_PREFIX + group.toUpperCase()))
+                            .collect(Collectors.toList());
+
+                    mappedAuthorities.addAll(groupAuthorities);
+                }
+            });
+            return mappedAuthorities;
+        });
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder()
-    {
-        return new BCryptPasswordEncoder();
-    }
+    // REMOVED AS WE ARE NOT USING IN-MEMORY AUTHENTICATION ANYMORE
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+//    {
+//        auth.inMemoryAuthentication().withUser(userConfigData.getUsername())
+//                // .password(userConfigData.getPassword()) // using BCrypted and then encrypted password with JCE
+//                .password(passwordEncoder().encode(
+//                        userConfigData.getPassword())) // using encrypted password with JCE, preferred way as it is
+//                // working in all cases, and the above on is not
+//                .roles(userConfigData.getRoles());
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder()
+//    {
+//        return new BCryptPasswordEncoder();
+//    }
 }
 
